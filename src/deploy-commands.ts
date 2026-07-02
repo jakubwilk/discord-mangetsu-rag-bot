066915253a46848e7ca07e2f6cdb17013862f910
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2026-07-02 20:15:29 +0200

    feat: update command deployment and event handling for improved functionality

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -2,11 +2,21 @@ import 'dotenv/config';
 
 import { REST, Routes } from 'discord.js';
 
+import * as chatCommand from './commands/chat';
+import * as configCommand from './commands/config';
+import * as statsCommand from './commands/stats';
+import * as statusCommand from './commands/status';
+
 const token = process.env.DISCORD_TOKEN!;
 const clientId = process.env.DISCORD_CLIENT_ID!;
 const guildId = process.env.DISCORD_GUILD_ID!;
 
-const commands: object[] = [];
+const commands: object[] = [
+  chatCommand.data.toJSON(),
+  configCommand.data.toJSON(),
+  statusCommand.data.toJSON(),
+  statsCommand.data.toJSON(),
+];
 
 const rest = new REST().setToken(token);
 
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,31 @@ import 'dotenv/config';
 
 import { Client, GatewayIntentBits } from 'discord.js';
 
+import * as interactionCreateEvent from './events/interactionCreate';
+import * as messageCreateEvent from './events/messageCreate';
 import * as readyEvent from './events/ready';
+import * as configService from './services/configService';
+
+configService.validateEnv();
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 client.once(readyEvent.name, (c) => {
   readyEvent.execute(c);
+});
+
+client.on(interactionCreateEvent.name, (interaction) => {
+  void interactionCreateEvent.execute(interaction);
+});
+
+client.on(messageCreateEvent.name, (message) => {
+  void messageCreateEvent.execute(message);
 });
 
 process.on('unhandledRejection', (error) => {
